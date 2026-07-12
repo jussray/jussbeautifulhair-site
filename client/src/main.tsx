1f431a5fd59ab51a5c8bcaffe7ef35337604d0d8
Juss <286642846+jussray@users.noreply.github.com>
2026-07-12 14:06:02 -0400

    Install storefront guardrails at app startup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { installJbhGuardrailRuntime } from "./config/visionGuardrails";
+
+installJbhGuardrailRuntime();
 
 if (!window.location.hash) {
   window.location.hash = "#/";
